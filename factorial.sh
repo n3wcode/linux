4ccb0651e1c07bb2e4e230c79b2a212e84fea109
n3wcode <trexnet@protonmail.com>
2021-06-12 22:07:06 -0400

    first commit

--- factorial.sh
+++ factorial.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-counter=$1
-factorial=1
-while [ $counter -gt 0 ]
-do
-	factorial=$(( $factorial * $counter ))
-	counter=$(( $counter -1 ))
-done
+# USAGE: ./factorial.sh [your_number]
+
+INPUT="$1"
+if test "$INPUT" = ""
+then
+	echo 'USAGE: "./factorial.sh [your_number]"'
+	exit 1
+else
+	counter=$1
+	factorial=1
+	while [ $counter -gt 0 ]
+	do
+		factorial=$(( $factorial * $counter ))
+		counter=$(( $counter -1 ))
+	done
+fi
 echo $factorial
